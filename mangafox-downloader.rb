8d175b64bfe5baeb7baf8a2a7cc9c7de5ba46b81
Jean-Franck Ballet <jean-franck@captive.fr>
2014-07-22 16:53:02 +0200

    All chapters of Death Note can be downloaded

--- mangafox-downloader.rb
+++ mangafox-downloader.rb
@@ -5,17 +5,32 @@ def read_url url
   Nokogiri::XML(open(url).read)
 end
 
-flux = read_url "http://mangafox.me/manga/death_note/v13/c110.5/1.html"
-flux.css('#top_center_bar .l select option')[0...-1].each do |option|
-  
-  page = read_url "http://mangafox.me/manga/death_note/v13/c110/#{option.attributes["value"].value}.html"
-  
-  src = page.css('#viewer img#image')[0].attributes["src"].value
-  extension = src.split('.')[src.split('.').count - 1]
+def get_base_url_chapter url
+  "#{url.split('/')[0...-1].join('/')}/"
+end
 
-  open("page-#{option.attributes["value"].value}.#{extension}",'wb') do |file|
-    p file
-    file << open(src).read
+Dir.mkdir('Death Note') unless File.exists?("Death Note")
+
+manga = Nokogiri::XML(open('http://mangafox.me/manga/death_note/').read)
+
+manga.css('.chlist li').each do |chapter|
+  name = chapter.css('.tips')[0].children[0].text
+  Dir.mkdir("Death Note/#{name}") unless File.exists?("Death Note/#{name}")
+  
+  chapter = chapter.css('.tips')[0].attributes["href"].value
+  base_url_chapter = get_base_url_chapter chapter
+  flux = read_url "#{base_url_chapter}1.html"
+  flux.css('#top_center_bar .l select option')[0...-1].each do |option|
+    
+    page = read_url "#{base_url_chapter}#{option.attributes["value"].value}.html"
+    
+    src = page.css('#viewer img#image')[0].attributes["src"].value
+    extension = src.split('.')[src.split('.').count - 1]
+
+    open("Death Note/#{name}/page-#{option.attributes["value"].value}.#{extension}",'wb') do |file|
+      p file
+      file << open(src).read
+    end
+
   end
-
 end
