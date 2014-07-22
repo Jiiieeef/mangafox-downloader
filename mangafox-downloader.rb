294db77cf1f291d010e3630fc213609f715393bf
Jean-Franck Ballet <jean-franck@captive.fr>
2014-07-22 16:27:32 +0200

    Create a method to read an url and get xml

--- mangafox-downloader.rb
+++ mangafox-downloader.rb
@@ -1,10 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
 
-flux = Nokogiri::XML(open("http://mangafox.me/manga/death_note/v13/c110.5/1.html").read)
+def read_url url
+  Nokogiri::XML(open(url).read)
+end
+
+flux = read_url "http://mangafox.me/manga/death_note/v13/c110.5/1.html"
 flux.css('#top_center_bar .l select option')[0...-1].each do |option|
   
-  page = Nokogiri::XML(open("http://mangafox.me/manga/death_note/v13/c110/#{option.attributes["value"].value}.html").read)
+  page = read_url "http://mangafox.me/manga/death_note/v13/c110/#{option.attributes["value"].value}.html"
   
   src = page.css('#viewer img#image')[0].attributes["src"].value
   extension = src.split('.')[src.split('.').count - 1]
