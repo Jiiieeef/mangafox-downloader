e91e0a09d2ef080f8fd9baebb6214ff0c4c6b69d
Jean-Franck Ballet <ballet.jean.franck@gmail.com>
2014-07-23 22:39:19 +0200

    Can download many manga with arguments

--- mangafox-downloader.rb
+++ mangafox-downloader.rb
@@ -10,37 +10,41 @@ def get_base_url_chapter url
   "#{url.split('/')[0...-1].join('/')}/"
 end
 
-manga_name = ARGV[0]
-manga_name_slug = manga_name.gsub(/\s+/, "_").downcase
-manga = read_url "http://mangafox.me/manga/#{manga_name_slug}"
+mangas = ARGV
 
-chapters = manga.css('.chlist li')
+mangas.each do |manga|
 
-if chapters.count > 0
+  manga_slug = manga.gsub(/\s+/, "_").downcase
+  manga_html = read_url "http://mangafox.me/manga/#{manga_slug}"
 
-  Dir.mkdir(manga_name) unless File.exists?(manga_name)
+  chapters = manga_html.css('.chlist li')
 
-  chapters.reverse.each do |chapter|
-    name = chapter.css('.tips')[0].children[0].text
-    Dir.mkdir("#{manga_name}/#{name}") unless File.exists?("#{manga_name}/#{name}")
-    
-    chapter = chapter.css('.tips')[0].attributes["href"].value
-    base_url_chapter = get_base_url_chapter chapter
-    flux = read_url "#{base_url_chapter}1.html"
-    flux.css('#top_center_bar .l select option')[0...-1].each do |option|
+  if chapters.count > 0
+
+    Dir.mkdir(manga) unless File.exists?(manga)
+
+    chapters.reverse.each do |chapter|
+      name = chapter.css('.tips')[0].children[0].text
+      Dir.mkdir("#{manga}/#{name}") unless File.exists?("#{manga}/#{name}")
       
-      page = read_url "#{base_url_chapter}#{option.attributes["value"].value}.html"
-      
-      src = page.css('#viewer img#image')[0].attributes["src"].value
-      extension = src.split('.')[src.split('.').count - 1]
+      chapter = chapter.css('.tips')[0].attributes["href"].value
+      base_url_chapter = get_base_url_chapter chapter
+      flux = read_url "#{base_url_chapter}1.html"
+      flux.css('#top_center_bar .l select option')[0...-1].each do |option|
+        
+        page = read_url "#{base_url_chapter}#{option.attributes["value"].value}.html"
+        
+        src = page.css('#viewer img#image')[0].attributes["src"].value
+        extension = src.split('.')[src.split('.').count - 1]
 
-      open("#{manga_name}/#{name}/page-#{option.attributes["value"].value}.#{extension}",'wb') do |file|
-        p file
-        file << open(src).read
+        open("#{manga}/#{name}/page-#{option.attributes["value"].value}.#{extension}",'wb') do |file|
+          p file
+          file << open(src).read
+        end
       end
     end
+    TerminalNotifier.notify("Download of \"#{manga}\" is over.", title: 'MangaFox Downloader', sound: 'default')
+  else
+    p "#{manga} not found :("
   end
-  TerminalNotifier.notify("Download of \"#{manga_name}\" is over.", title: 'MangaFox Downloader', sound: 'default')
-else
-  p "#{manga_name} not found :("
 end
