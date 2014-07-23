44cbf4224d1a498e377a4e9655f84083ac22738c
Jean-Franck Ballet <ballet.jean.franck@gmail.com>
2014-07-23 22:01:13 +0200

    Create folder only if the manga give in argument exist

--- mangafox-downloader.rb
+++ mangafox-downloader.rb
@@ -13,28 +13,30 @@ manga_name = ARGV[0]
 manga_name_slug = manga_name.gsub(/\s+/, "_").downcase
 manga = read_url "http://mangafox.me/manga/#{manga_name_slug}"
 
+chapters = manga.css('.chlist li')
 
+if chapters.count > 0
 
-Dir.mkdir(manga_name) unless File.exists?(manga_name)
+  Dir.mkdir(manga_name) unless File.exists?(manga_name)
 
-manga.css('.chlist li').reverse.each do |chapter|
-  name = chapter.css('.tips')[0].children[0].text
-  Dir.mkdir("#{manga_name}/#{name}") unless File.exists?("#{manga_name}/#{name}")
-  
-  chapter = chapter.css('.tips')[0].attributes["href"].value
-  base_url_chapter = get_base_url_chapter chapter
-  flux = read_url "#{base_url_chapter}1.html"
-  flux.css('#top_center_bar .l select option')[0...-1].each do |option|
+  chapters.reverse.each do |chapter|
+    name = chapter.css('.tips')[0].children[0].text
+    Dir.mkdir("#{manga_name}/#{name}") unless File.exists?("#{manga_name}/#{name}")
     
-    page = read_url "#{base_url_chapter}#{option.attributes["value"].value}.html"
-    
-    src = page.css('#viewer img#image')[0].attributes["src"].value
-    extension = src.split('.')[src.split('.').count - 1]
+    chapter = chapter.css('.tips')[0].attributes["href"].value
+    base_url_chapter = get_base_url_chapter chapter
+    flux = read_url "#{base_url_chapter}1.html"
+    flux.css('#top_center_bar .l select option')[0...-1].each do |option|
+      
+      page = read_url "#{base_url_chapter}#{option.attributes["value"].value}.html"
+      
+      src = page.css('#viewer img#image')[0].attributes["src"].value
+      extension = src.split('.')[src.split('.').count - 1]
 
-    open("#{manga_name}/#{name}/page-#{option.attributes["value"].value}.#{extension}",'wb') do |file|
-      p file
-      file << open(src).read
+      open("#{manga_name}/#{name}/page-#{option.attributes["value"].value}.#{extension}",'wb') do |file|
+        p file
+        file << open(src).read
+      end
     end
-
   end
 end
