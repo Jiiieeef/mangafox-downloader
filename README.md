0ec566b1e23e23b5c380d1c730f9e8dd437f3582
Jean-Franck Ballet <jean-franck@captive.fr>
2014-09-12 12:04:55 +0200

    Remove the ability to download several mangas in once

--- README.md
+++ README.md
@@ -3,11 +3,9 @@ Allows you to download manga on mangafox.me
 #How to use it
 
 ```shell
-$ ruby mangafox-downloader.rb "Death Note" "Hunter X Hunter"
+$ ruby mangafox-downloader.rb "Death Note"
 ```
 
-When downloads are finished, scans are in folders:
-- Downloads/Death Note
-- Downloads/Hunter X Hunter
+When downloads are finished, scans are in folders: Downloads/Death Note
 
 Inspired by the great [@gntics](https://github.com/gntics)
--- mangafox-downloader.rb
+++ mangafox-downloader.rb
@@ -34,44 +34,39 @@ download_path = $config["path_to_create"]
 
 Dir.mkdir(download_path) unless Dir.exist?(download_path)
 
-mangas = ARGV
+manga_slug = slugify ARGV[0]
+manga_html = read_url "http://mangafox.me/manga/#{manga_slug}"
 
-mangas.each do |manga|
+chapters = manga_html.css('.chlist li')
 
-  manga_slug = slugify manga
-  manga_html = read_url "http://mangafox.me/manga/#{manga_slug}"
+if chapters.count > 0
 
-  chapters = manga_html.css('.chlist li')
+  Dir.mkdir("#{download_path}/#{manga}") unless File.exist?("#{download_path}/#{manga}")
 
-  if chapters.count > 0
-
-    Dir.mkdir("#{download_path}/#{manga}") unless File.exist?("#{download_path}/#{manga}")
-
-    chapters.reverse.each do |chapter|
-      name = chapter.css('.tips')[0].children[0].text
-      title = chapter.css('.title')[0].children[0].text
-      name += " - #{title}" unless title.nil?
-      Dir.mkdir("#{download_path}/#{manga}/#{name}") unless File.exist?("#{download_path}/#{manga}/#{name}")
+  chapters.reverse.each do |chapter|
+    name = chapter.css('.tips')[0].children[0].text
+    title = chapter.css('.title')[0].children[0].text
+    name += " - #{title}" unless title.nil?
+    Dir.mkdir("#{download_path}/#{manga}/#{name}") unless File.exist?("#{download_path}/#{manga}/#{name}")
+    
+    chapter = chapter.css('.tips')[0].attributes["href"].value
+    base_url_chapter = get_base_url_chapter chapter
+    flux = read_url "#{base_url_chapter}1.html"
+    flux.css('#top_center_bar .l select option')[0...-1].each do |option|
       
-      chapter = chapter.css('.tips')[0].attributes["href"].value
-      base_url_chapter = get_base_url_chapter chapter
-      flux = read_url "#{base_url_chapter}1.html"
-      flux.css('#top_center_bar .l select option')[0...-1].each do |option|
-        
-        page = read_url "#{base_url_chapter}#{option.attributes["value"].value}.html"
-        
-        src = page.css('#viewer img#image')[0].attributes["src"].value
-        extension = src.split('.')[src.split('.').count - 1]
+      page = read_url "#{base_url_chapter}#{option.attributes["value"].value}.html"
+      
+      src = page.css('#viewer img#image')[0].attributes["src"].value
+      extension = src.split('.')[src.split('.').count - 1]
 
-        open("#{download_path}/#{manga}/#{name}/page-#{option.attributes["value"].value}.#{extension}",'wb') do |file|
-          p file
-          file << open(src).read
-        end
+      open("#{download_path}/#{manga}/#{name}/page-#{option.attributes["value"].value}.#{extension}",'wb') do |file|
+        p file
+        file << open(src).read
       end
-      zip_chapter Dir["#{download_path}/#{manga}/#{name}/*"], "#{download_path}/#{manga}/#{name}" if $config["zip"]["should_archive"]
     end
-    TerminalNotifier.notify("Download of \"#{manga}\" is over.", title: 'MangaFox Downloader', sound: 'default') if $config["notification"]["should_notify"]
-  else
-    p "#{manga} not found :("
+    zip_chapter Dir["#{download_path}/#{manga}/#{name}/*"], "#{download_path}/#{manga}/#{name}" if $config["zip"]["should_archive"]
   end
+  TerminalNotifier.notify("Download of \"#{manga}\" is over.", title: 'MangaFox Downloader', sound: 'default') if $config["notification"]["should_notify"]
+else
+  p "#{manga} not found :("
 end
